9461fc8cab52a21a4cefd14284d8660b2c9541c1
Pablo Henrick Diniz <phydokz@hotmail.com>
2015-07-25 19:31:57 -0400

    first upload

--- Overlap.js
+++ Overlap.js
@@ -15,22 +15,25 @@ var Overlap = {
         var maxY = cpMaxy + circle.radius;
         return (((cc.x <= va.x && cc.x >= minX) || (cc.x >= va.x && cc.x <= maxX)) && (cc.t <= maxY && cc.y >= minY));
     },
-    circlePoint:function(circle,pt){
-        var distance = Math.distance(pt, {x:circle.x,y:circle.y});
+    circlePoint: function (circle, pt) {
+        var distance = Math.distance(pt, {x: circle.x, y: circle.y});
         return distance <= circle.radius;
     },
-    shapePoint:function(shape,point){
-        switch(shape.type){
+    shapePoint: function (shape, point) {
+        switch (shape.type) {
             case 'rect':
-                return this.rectPoint(shape,point);
+                return this.rectPoint(shape, point);
                 break;
             case 'circle':
-                return this.circlePoint(shape,point);
+                return this.circlePoint(shape, point);
                 break;
+            case 'polygon':
+                return this.pointOnPolygon(shape, point);
+            break;
         }
         return false;
     },
-    rectPoint:function(rect,pt){
+    rectPoint: function (rect, pt) {
         var xo = rect.x;
         var yo = rect.y;
         var xf = rect.x + rect.width;
@@ -38,25 +41,25 @@ var Overlap = {
         return (xo <= pt.x && yo <= pt.y && xf >= pt.x && yf >= pt.y);
     },
     circleRect: function (circle, rect) {
-        var pa = {x:rect.x,y:rect.y};
-        var pc = {x:rect.x+rect.width,y:rect.y+rect.height};
+        var pa = {x: rect.x, y: rect.y};
+        var pc = {x: rect.x + rect.width, y: rect.y + rect.height};
 
-        if(circle.x >= pa.x && circle.x <= pc.x && circle.y >= pa.y && circle.y <= pc.y){
+        if (circle.x >= pa.x && circle.x <= pc.x && circle.y >= pa.y && circle.y <= pc.y) {
             return true;
         }
 
-        var pb = {x:rect.x+rect.width,y:rect.y};
-        var pd = {x:rect.x,y:rect.y+rect.height};
-        var la = {pa:pa,pb:pb};
-        var lb = {pa:pb,pb:pc};
-        var lc = {pa:pc,pb:pd};
-        var ld = {pa:pd,pb:pa};
+        var pb = {x: rect.x + rect.width, y: rect.y};
+        var pd = {x: rect.x, y: rect.y + rect.height};
+        var la = {pa: pa, pb: pb};
+        var lb = {pa: pb, pb: pc};
+        var lc = {pa: pc, pb: pd};
+        var ld = {pa: pd, pb: pa};
 
-        return(
-        this.circleLine(circle,la) ||
-        this.circleLine(circle,lb) ||
-        this.circleLine(circle,lc) ||
-        this.circleLine(circle,ld)
+        return (
+        this.circleLine(circle, la) ||
+        this.circleLine(circle, lb) ||
+        this.circleLine(circle, lc) ||
+        this.circleLine(circle, ld)
         );
     },
     rectIntersectRect: function (rectA, rectB) {
@@ -69,6 +72,14 @@ var Overlap = {
         var x1b = rectB.x + rectB.width;
         var y1b = rectB.y + rectB.height;
         return !(x0a > x1b || x1a < x0b || y0a > y1b || y1a < y0b);
+    },
+    pointOnPolygon: function (shape, pt) {
+        var poly = shape.vertices;
+        for (var c = false, i = -1, l = poly.length, j = l - 1; ++i < l; j = i)
+            ((poly[i].y <= pt.y && pt.y < poly[j].y) || (poly[j].y <= pt.y && pt.y < poly[i].y))
+            && (pt.x < (poly[j].x - poly[i].x) * (pt.y - poly[i].y) / (poly[j].y - poly[i].y) + poly[i].x)
+            && (c = !c);
+        return c;
     }
 };
 
